01b41b5d7bd5ed8efde817ed1a42ad45b9361fcc
amnrwt4@gmail.com <amnrwt4gmail.com>
2025-11-10 19:09:17 +0530

    FrontEnd_URL

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const cors = require('cors');
 app.use(
   cors({
     origin: [
-      process.env.FRONTEND_URl,
+      process.env.FRONTEND_URL,
       'http://localhost:5173',
     ],
     credentials: true,
