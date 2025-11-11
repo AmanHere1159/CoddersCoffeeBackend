9547781a4afd00d81aad21ce94936b4220c72a69
amnrwt4@gmail.com <amnrwt4gmail.com>
2025-11-11 13:00:49 +0530

    blabla

--- Model/signupModel.js
+++ Model/signupModel.js
@@ -4,8 +4,8 @@ const signupSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    address: { type: Number, required: true },
-   landmark: { type: Number, required: true },
+    address: { type: String, required: true },
+   landmark: { type: String, required: true },
     pincode: { type: Number, required: true },
 },
   { timestamps: true })
