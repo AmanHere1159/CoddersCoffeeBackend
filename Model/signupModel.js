c30a9da7978510188e9cf57701241e0e33943c93
amnrwt4@gmail.com <amnrwt4gmail.com>
2025-11-11 12:57:23 +0530

    SignupChangesAgain

--- Model/signupModel.js
+++ Model/signupModel.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose');
 const signupSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
-    country: { type: String, required: true },
-    zipcode: { type: Number, required: true },
+    password: { type: String, required: true },
+    address: { type: Number, required: true },
+   landmark: { type: Number, required: true },
+    pincode: { type: Number, required: true },
 },
   { timestamps: true })
   const Signup = mongoose.model("Signup", signupSchema);
